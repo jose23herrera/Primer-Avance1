f321ce9202cd5c0cc898f57bf6dc78b4b1e14d7c
MB170688 <dariomartinez888@yahoo.com>
2023-10-24 13:24:19 -0600

    Arreglos en javascript

---
+++ js/autores.js
@@ -0,0 +1,158 @@
+const autores = [
+    {
+        nombre: "Gabriel Garcia Marquez",
+        sexo: "masculino",
+        nacionalidad: "colombiano",
+        idioma: "español",
+        biografia: "Gabriel José de la Concordia García Márquez (Aracataca, Magdalena, Colombia, 6 de marzo de 1927-Ciudad de México, 17 de abril de 2014) fue un escritor y periodista colombiano. Reconocido principalmente por sus novelas y cuentos, también escribió narrativa de no ficción, discursos, reportajes, críticas cinematográficas y memorias. Estudió derecho y periodismo en la Universidad Nacional de Colombia e inicio sus primeras colaboraciones periodísticas en el diario El Espectador. Fue conocido como Gabo, y familiarmente y por sus amigos como Gabito. En 1982 recibió el Premio Nobel de Literatura​ «por sus novelas e historias cortas, en las que lo fantástico y lo real se combinan en un mundo ricamente compuesto de imaginación, lo que refleja la vida y los conflictos de un continente».",
+        dir: "pagina5.html",
+        img: "img/autor1.jpg",
+        libros: [
+            {
+                titulo: "Cien años de soledad",
+                generos: "Novela, Realismo mágico, Ficción, Alta fantasía, Saga familiar",
+                sinopsis: "El libro narra la historia de la familia Buendía a lo largo de siete generaciones en el pueblo ficticio de Macondo.",
+                anio: "1967"
+            },
+            {
+                titulo: "Crónica de una muerte anunciada",
+                generos: "Novela, Policial",
+                sinopsis: "El matrimonio de Bayardo San Román y Ángela Vicario",
+                anio: "1981"
+            },
+        ]
+    },
+    {
+        nombre: "J.R.R. Tolkien",
+        sexo: "masculino",
+        nacionalidad: "británico",
+        idioma: "inglés",
+        biografia: "John Ronald Reuel Tolkien, CBE (Bloemfontein, Sudáfrica; 3 de enero de 1892-Bournemouth, Reino Unido; 2 de septiembre de 1973), más conocido como J. R. R. Tolkien, fue un escritor, poeta, filólogo y profesor universitario británico, conocido por ser el autor de las novelas clásicas de fantasía El Hobbit, El Señor de los Anillos y El Silmarillion. Fue nombrado Comendador de la Orden del Imperio Británico por la Reina Isabel II el 28 de marzo de 1969. Fue un filólogo y profesor de anglosajón en la Universidad de Oxford y profesor de lengua y literatura inglesa en la Universidad de Leeds.",
+        dir: "pagina5.html",
+        img: "img/autor2.jpg",
+        libros: [
+            {
+                titulo: "El Hobbit",
+                generos: "Novela, Fantasía, Aventura",
+                sinopsis: "La historia sigue las aventuras del hobbit Bilbo Bolsón mientras se embarca en un viaje inesperado acompañado por un grupo de enanos y un mago llamado Gandalf.",
+                anio: "1937"
+            },
+            {
+                titulo: "El Señor de los Anillos",
+                generos: "Novela, Fantasía, Alta fantasía",
+                sinopsis: "La trilogía sigue las épicas aventuras de Frodo Bolsón mientras intenta destruir el Anillo Único y derrotar al Señor Oscuro Sauron.",
+                anio: "1954-1955"
+            },
+            {
+                titulo: "El Silmarillion",
+                generos: "Novela, Fantasía, Alta fantasía, Mitología",
+                sinopsis: "El libro relata la historia de la creación del mundo y las primeras edades de la Tierra Media, incluyendo la lucha entre el bien y el mal.",
+                anio: "1977"
+            },
+        ]
+    },
+    {
+        nombre: "George Orwell",
+        sexo: "masculino",
+        nacionalidad: "británico",
+        idioma: "inglés",
+        biografia: "George Orwell, seudónimo de Eric Arthur Blair (Motihari, India Británica; 25 de junio de 1903-Londres, Reino Unido; 21 de enero de 1950), fue un escritor, periodista y ensayista británico. Es conocido por sus obras críticas con la sociedad totalitaria y los regímenes autoritarios, especialmente por las novelas '1984' y 'Rebelión en la granja'. Orwell se convirtió en una figura destacada de la literatura del siglo XX debido a su compromiso político y su estilo claro y directo de escritura.",
+        dir: "pagina5.html",
+        img: "img/autor3.jpg",
+        libros: [
+            {
+                titulo: "1984",
+                generos: "Novela, Distopía, Ciencia ficción",
+                sinopsis: "La novela presenta un mundo totalitario en el que el Partido controla todos los aspectos de la vida y la realidad, y donde la vigilancia y la represión son omnipresentes.",
+                anio: "1949"
+            },
+            {
+                titulo: "Rebelión en la granja",
+                generos: "Novela, Sátira, Alegoría",
+                sinopsis: "La historia narra la revuelta de los animales de una granja contra el cruel dueño humano, pero la revolución se corrompe con el tiempo.",
+                anio: "1945"
+            },
+            {
+                titulo: "Homenaje a Cataluña",
+                generos: "Memorias, Guerra civil española",
+                sinopsis: "Orwell relata su experiencia como combatiente en la Guerra Civil Española, donde luchó del lado republicano.",
+                anio: "1938"
+            },
+        ]
+    },
+    {
+        nombre: "Alex Mirez",
+        sexo: "femenino",
+        nacionalidad: "venezolana",
+        idioma: "español",
+        biografia: "Alex Mirez es una autora y escritora venezolana conocida por su obra en diversos géneros literarios. Su estilo literario es conocido por su narrativa intrigante y su capacidad para explorar temas oscuros y misteriosos. Sus obras han ganado reconocimiento por su capacidad para sumergir a los lectores en mundos llenos de suspense y emoción.",
+        dir: "pagina5.html",
+        img: "img/autor4.jpg",
+        libros: [
+            {
+                titulo: "Damián un secreto oscuro y perverso",
+                generos: "Novela, Misterio, Suspense",
+                sinopsis: "Padme ha vivido durante toda su vida en el pueblo de Asfil. Es una chica normal, buena. O eso parece. En el fondo tiene varios secretos, y uno de ellos se llama Damián. Es su peculiar vecino, que siempre le dio la impresión de estar ocultando algo y a quien ella trató de descifrar sin éxito. Un día, Damián desaparece y su vida sigue un curso corriente, pero meses después vuelve, y con él regresa la peligrosa curiosidad que Padme tanto intentó reprimir. Ella decide, finalmente, seguirlo. Es entonces cuando descubre eso que tanto Damián como Asfil, han intentado...¿esconder? ¿Y si te preparas para adentrarte en este perverso mundo? Aquí no existen la moral ni la cordura. Si decides dejar eso atrás, entonces ellos te recibirán...",
+                anio: "2022"
+            },
+        ]
+    },
+    {
+        nombre: "J.K. Rowling",
+        sexo: "femenino",
+        nacionalidad: "británica",
+        idioma: "inglés",
+        biografia: "Joanne Rowling, más conocida como J.K. Rowling, es una autora británica famosa por su serie de novelas de Harry Potter. Nacida en Yate, Gloucestershire, el 31 de julio de 1965, Rowling se convirtió en una de las escritoras más exitosas de la historia. Sus libros han vendido más de 500 millones de copias en todo el mundo y han sido adaptados a películas exitosas. Su obra ha tenido un impacto significativo en la literatura y la cultura pop.",
+        dir: "pagina5.html",
+        img: "img/autor5.jpg",
+        libros: [
+            {
+                titulo: "Harry Potter y la piedra filosofal",
+                generos: "Novela, Fantasía, Aventura",
+                sinopsis: "La historia sigue a un joven mago llamado Harry Potter mientras descubre su legado mágico y asiste a la escuela de magia de Hogwarts.",
+                anio: "1997"
+            },
+            {
+                titulo: "Harry Potter y la cámara secreta",
+                generos: "Novela, Fantasía, Aventura",
+                sinopsis: "Harry Potter regresa a su segundo año en Hogwarts y descubre un misterio relacionado con una cámara secreta y un monstruo que amenaza a los estudiantes.",
+                anio: "1998"
+            },
+            {
+                titulo: "Harry Potter y el prisionero de Azkaban",
+                generos: "Novela, Fantasía, Aventura",
+                sinopsis: "Harry Potter se entera de un peligroso prisionero que ha escapado de Azkaban, la prisión de los magos, y está en busca de venganza.",
+                anio: "1999"
+            },
+        ]
+    },
+    {
+        nombre: "Suzanne Collins",
+        sexo: "femenino",
+        nacionalidad: "estadounidense",
+        idioma: "inglés",
+        biografia: "Suzanne Collins es una autora y guionista estadounidense conocida por su exitosa trilogía de Los Juegos del Hambre. Nacida el 10 de agosto de 1962 en Hartford, Connecticut, Collins ha recibido reconocimiento mundial por su contribución a la literatura juvenil y la ciencia ficción. Sus obras han sido adaptadas a películas y han alcanzado un gran éxito comercial.",
+        dir: "pagina5.html",
+        img: "img/autor6.jpg",
+        libros: [
+            {
+                titulo: "Los Juegos del Hambre",
+                generos: "Novela, Ciencia ficción, Distopía",
+                sinopsis: "En un futuro distópico, Katniss Everdeen se ve obligada a participar en un reality show mortal en el que los tributos luchan por sobrevivir y ganar la fama y la riqueza.",
+                anio: "2008"
+            },
+            {
+                titulo: "En llamas",
+                generos: "Novela, Ciencia ficción, Distopía",
+                sinopsis: "Katniss y Peeta deben enfrentarse nuevamente a los Juegos del Hambre, pero esta vez con reglas más mortales y desafiantes.",
+                anio: "2009"
+            },
+            {
+                titulo: "Sinsajo",
+                generos: "Novela, Ciencia ficción, Distopía",
+                sinopsis: "La rebelión contra el Capitolio se intensifica, y Katniss se convierte en el símbolo de la resistencia mientras lucha por sobrevivir y derrocar al régimen opresor.",
+                anio: "2010"
+            },
+        ]
+    }
+];
---
+++ js/carrusel.js
@@ -0,0 +1,27 @@
+let slider = document.querySelector(".slider-contenedor");
+let sliderIndividual = document.querySelectorAll(".contenido-slider");
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 6000;
+
+window.addEventListener('resize', function () {
+    let width = sliderIndividual[0].clientWidth;
+});
+
+function slides () {
+    slider.style.transform = "translate(" +(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if (contador == sliderIndividual.length) {
+        setTimeout( function() {
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador = 1;
+        }, 1500);
+    }
+}
+
+setInterval(function () {
+    slides();
+}, intervalo);
